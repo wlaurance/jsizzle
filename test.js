46276f9e21f82d089a0358835468d2e702ec343d
wlaurance <w.laurance@gmail.com>
2013-07-30 11:50:50 -0400

    Init jsizzle

---
+++ test.js
@@ -0,0 +1,11 @@
+var spawn = require('child_process').spawn,
+    should = require('should');
+
+describe('jsizzle', function(){
+  describe('API', function(){
+    it('should return a tweet list');
+  });
+  describe('bin', function(){
+    it('should print one tweet');
+  });
+});
